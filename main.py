d84b6ed057f49407476d1b9d603ebccf8eb8c75e
Veronica Timofte <veronica.timofte@192.168.1.102>
2021-06-22 18:22:48 +0100

    added a .csv writer

--- main.py
+++ main.py
@@ -20,11 +20,6 @@ csv_reader = reader(input_file)
 next(csv_reader, None)
 
 
-# Defining the output file, the output's file headers and the writer we'll use to create the file
-output_file = open("translated_text.csv", "w", newline='\n')
-headers = ["Country Code", "Detected Source Language", "Keyword", "Translated Keyword", "Translated", "Volume"]
-writer = csv.DictWriter(output_file, fieldnames=headers)
-
 allowed_languages = {
     "GB": ["en"],
     "US": ["en", "es", "fr"],
@@ -45,18 +40,35 @@ def main():
             Taking a  keyword found in input_file, use the translator object to translate the keyword
             which will be written in a new file
         """
-    # Searching for keywords in input file
-    for row in csv_reader:
-        keyword = row[1]
-        country_code = row[14]
-        if country_code in allowed_languages:
-            source_language = allowed_languages[country_code][0]
+    # Defining the output file, the output's file headers and the writer we'll use to create the file
+    with open("translated_text.csv", "w", newline='\n') as output_file:
+        headers = ["Country Code", "Detected Source Language", "Keyword", "Translated Keyword", "Volume"]
+        writer = csv.writer(output_file, delimiter=',')
+        writer.writerow(headers)
 
-            print(row)
-            print(f"Source Language: {source_language}")
-            print(f"Keyword: {keyword}")
-            result = translate.translate_text(Text=keyword, SourceLanguageCode=source_language, TargetLanguageCode="EN")
-            print(f'Translated text: {result["TranslatedText"]}')
+        # Searching for keywords in input file
+        for row in csv_reader:
+            keyword = row[1]
+            country_code = row[14]
+            volume = row[5]
+
+            if country_code in allowed_languages:
+                source_language = allowed_languages[country_code][0]
+
+                print(row)
+                print(f"Source Language: {source_language}")
+                print(f"Keyword: {keyword}")
+                result = translate.translate_text(Text=keyword, SourceLanguageCode=source_language, TargetLanguageCode="EN")
+                print(f'Translated text: {result["TranslatedText"]}')
+
+                # Using the writer, we are writing the new 3 columns to the file
+                new_row = []
+                new_row.insert(0, country_code)
+                new_row.insert(1, source_language)
+                new_row.insert(2, keyword)
+                new_row.insert(3, result["TranslatedText"])
+                new_row.insert(4, volume)
+                writer.writerow(new_row)
 
 
 if __name__ == '__main__':
